19d249584c68b515a4ea13909555b40e4c7ecfa6
AreeyaC <86303607+AreeyaC@users.noreply.github.com>
2021-07-21 19:37:10 +0700

    [U5] Update Book APi

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { json, urlencoded } = require('body-parser')
 
-const { readBooks, readBook, createBook, replaceBook } = require('./feature/book-controller')
+const { readBooks, readBook, createBook, replaceBook, updateBook } = require('./feature/book-controller')
 
 const app = express()
 
@@ -15,5 +15,6 @@ app.get('/books', readBooks)
 app.get('/books/:isbn13', readBook)
 app.post('/books/', createBook)
 app.put('/books/:isbn13', replaceBook)
+app.patch('/books/:isbn13', updateBook)
 
 module.exports = { app }
