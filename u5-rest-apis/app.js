03b48202a0c807ba20732cb7e25a22ffc0c24335
AreeyaC <86303607+AreeyaC@users.noreply.github.com>
2021-07-22 20:15:16 +0700

    [U5] Covid controller

--- u5-rest-apis/app.js
+++ u5-rest-apis/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { json, urlencoded } = require('body-parser')
 
 const { readBooks, readBook, createBook, replaceBook, updateBook, deleteBook } = require('./feature/book-controller')
+const { readCovidR, readCovid, createCovid, updateCovid, deleteCovid } = require('./feature/covid-controller')
 
 const app = express()
 
@@ -16,6 +17,11 @@ app.get('/books/:isbn13', readBook)
 app.post('/books/', createBook)
 app.put('/books/:isbn13', replaceBook)
 app.patch('/books/:isbn13', updateBook)
-app.patch('/books/:isbn13', deleteBook)
+app.delete('/books/:isbn13', deleteBook)
+app.get('/covid', readCovidR)
+app.get('/covid/:stateId', readCovid)
+app.post('/covid/', createCovid)
+app.patch('/covid/:stateId', updateCovid)
+app.delete('/covid/:stateId', deleteCovid)
 
 module.exports = { app }
