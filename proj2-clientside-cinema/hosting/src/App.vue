45ab9c04c00ea216b34d78de49f0bb343c6b4246
AreeyaC <86303607+AreeyaC@users.noreply.github.com>
2021-10-10 22:07:46 +0700

    App Layout

--- proj2-clientside-cinema/hosting/src/App.vue
+++ proj2-clientside-cinema/hosting/src/App.vue
@@ -1,9 +1,155 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="has-background-light" >
+    <b-navbar class="navbar is-dark">
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <p class="is-size-3 has-text-weight-bold">BECinema</p>
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="/cinemas">
+                Cinemas
+            </b-navbar-item>
+            <b-navbar-item href="/films">
+                Films
+            </b-navbar-item>
+        </template>
+
+        <template #end>
+          <b-icon class="position-relative m-4" icon="map-marker-radius-outline "></b-icon>
+          <b-navbar-dropdown label="Phitsanulok">
+                <b-navbar-item href="#">
+                    Bangkok
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Chiang Mai
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Phitsanulok
+                </b-navbar-item>
+            </b-navbar-dropdown>
+            <b-navbar-dropdown>
+              <template #label>
+                <img src="images\accounts\chaz.jpg"
+                alt="Chaz"
+                style="border-radius: 50%"/>
+                <p class="ml-2">Chaz</p>
+                </template>
+                <b-navbar-item href="/tickets">
+                    My Tickets
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+    </b-navbar>
+
+    <div class="container py-5">
+      <router-view />
+      <div class="m-6 p-6" />
     </div>
-    <router-view/>
+
+    <div class="layout-footer has-text-light has-background-dark py-5">
+      <div class="container">
+        <ol>
+          <li>
+            <router-link :to="{ name: 'CinemaList' }">
+              Cinema List
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'CinemaDetails',
+                params: { slug: 'phitsanulok-bec-auditorium' },
+              }"
+            >
+              Cinema Details
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'FilmList' }">
+              Film List
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'FilmDetails',
+                params: { slug: 'black-pink-movie' },
+              }"
+            >
+              Film Details
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{
+                name: 'Book',
+                params: {
+                  slug: 'phitsanulok-bec-auditorium,1,1628863200',
+                },
+              }"
+            >
+              Booking Form
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'Tickets' }">
+              My Tickets
+            </router-link>
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  min-height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+@import './main.scss';
+
+.image {
+  display: inline-block;
+  vertical-align: bottom;
+}
+
+.layout-footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.layout-footer ol {
+  margin: 0;
+  padding: 0 0 0 1rem;
+  column-count: 3;
+  column-gap: 2rem;
+}
+
+.layout-footer li {
+  padding-left: 0.5rem;
+  padding-top: 0.25rem;
+}
+
+.layout-footer ol a,
+.layout-footer ol a:hover {
+  background: inherit;
+  color: $light;
+  font-size: 1rem;
+  font-weight: bold;
+  text-decoration: underline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+</style>
